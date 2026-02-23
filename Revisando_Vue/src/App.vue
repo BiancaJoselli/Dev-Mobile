c912af48e13a2476aa92324d086e159fc3effc1f
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-02-23 15:20:23 -0300

    FEAT: revisando vue

---
+++ Revisando_Vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import AppInput from './components/AppInput.vue';
+
+const name = ref('');
+const email = ref('');
+</script>
+
+<template>
+  <AppInput v-model="name" label="Nome" placeholder="Digite seu nome" />
+
+  <AppInput
+    v-model="email"
+    label="E-mail"
+    type="email"
+    placeholder="seu@email.com"
+  />
+
+  <p>Nome: {{ name }}</p>
+  <p>E-mail: {{ email }}</p>
+</template>
