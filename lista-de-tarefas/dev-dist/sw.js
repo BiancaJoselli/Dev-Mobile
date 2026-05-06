6b4a9fb7533b8361b1467b5d08f54404ee694fb9
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-05-06 14:58:02 -0300

    TEST: Rodando o docker e o projeto

--- lista-de-tarefas/dev-dist/sw.js
+++ lista-de-tarefas/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4rbhg3hv1t"
+    "revision": "0.oo105hso2ic"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
